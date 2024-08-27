63e362c1e3919c3fbb06aa380364e9212b50088e
Omneya Osman <omneyaAhm@banquemisr.com>
2024-08-28 00:32:14 +0300

    add image feature

--- app/src/main/java/com/omneya/test/MainActivity.kt
+++ app/src/main/java/com/omneya/test/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
@@ -39,10 +45,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()){
+        Text(
+            text = "Hello Hello $name!",
+            modifier = modifier.clickable {
+                Analytics.trackEvent("My custom event")
+                //            Crashes.generateTestCrash()
+                //            throw Exception("noo")
+            }
+        )
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription ="Image" )
+    }
 }
 
 @Preview(showBackground = true)
